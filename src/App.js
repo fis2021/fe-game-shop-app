84a8b98b11aab2b9839388b453c2f49570165c07
Gheata Narcis <narcis_alexinho@yahoo.com>
2021-05-15 15:29:30 +0300

    Mock design components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import {useSelector} from "./stateManager";
 
+import { InstagramOutlined } from "@ant-design/icons";
+import {Button} from "antd";
+
 function App() {
     const testing = useSelector(state => state.testing);
     const person = testing[0];
@@ -7,6 +10,12 @@ function App() {
     <div className="App">
         Person: {person.name}
         <div className="testing">Limegreen</div>
+        <InstagramOutlined />
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
     </div>
   );
 }
