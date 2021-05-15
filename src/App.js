f8486e499a5d2a7289c2e5fdf8057bcd8ae4d829
Gheata Narcis <narcis_alexinho@yahoo.com>
2021-05-15 15:04:35 +0300

    Mock styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
         Person: {person.name}
+        <div className="testing">Limegreen</div>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
 
+import './styles/index.scss';
+
 import { getStore } from './stateManager';
 import { StateProvider } from 'redux-state-manager';
 
