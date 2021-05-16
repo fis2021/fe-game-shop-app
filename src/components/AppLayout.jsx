d3a8dc78640211b905fee700510df7b14a355884
Kriss Gardner <krissrobert12@gmail.com>
2021-05-16 17:05:03 +0300

    Create app layout component

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,19 @@
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const AppLayout = ({menu, content}) => {
+    return <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            {menu}
+        </Header>
+        <Content className="site-layout">
+            <div className="site-layout__background">
+                { content }
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Kriss & Narcis © 2020 | Built with style</Footer>
+    </Layout>
+}
+
+export default AppLayout;
