23440ebbea085aa8636edd8ec42a3f5f52dd57b3
Kriss Gardner <krissrobert12@gmail.com>
2021-05-16 21:02:43 +0300

    Display game categories

--- src/modules/Home/GameTable.jsx
+++ src/modules/Home/GameTable.jsx
@@ -2,20 +2,32 @@ import { Table } from "antd";
 import { useSelector } from "stateManager";
 import columnSearchMixin from "./columnSearchMixin";
 
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a, b) => (a.username > b.username ? 1 : -1),
-        ...columnSearchMixin('username'),
-    },
-];
-
-
 const GameTable = () => {
     const games = useSelector(state => state.games);
-    return <Table dataSource={games} columns={columns} />;
+    const categories = useSelector(state => state.categories);
+
+    return <Table dataSource={games} columns={[
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            sorter: (a, b) => (a.username > b.username ? 1 : -1),
+            ...columnSearchMixin('username'),
+        },
+        {
+            title: 'Category',
+            render: (any, record) => {
+                const result = [];
+                record.categories.forEach(id => {
+                    const item = categories.find(el => el.id === id);
+                    if (item) {
+                        result.push(item);
+                    }
+                })
+                return result.map(item => item.name).join(',');
+            }
+        }
+    ]} />;
 }
 
 export default GameTable;
